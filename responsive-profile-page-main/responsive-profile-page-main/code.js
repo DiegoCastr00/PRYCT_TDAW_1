c892138427bf45dd1a00ae0a4d44122c51d03b44
DiegoCastr00 <124998012+DiegoCastr00@users.noreply.github.com>
2023-03-08 16:17:12 -0600

    Delete responsive-profile-page-main directory

--- responsive-profile-page-main/responsive-profile-page-main/code.js
+++
@@ -1,9 +0,0 @@
-function myFunction(element) {
-    if (element.innerHTML == 'Follow +')
-    {
-        element.innerHTML = 'Followed ✓';
-        element.style.fontsize = '2vh';
-    }
-    else 
-        element.innerHTML = 'Follow +'; 
-  }
