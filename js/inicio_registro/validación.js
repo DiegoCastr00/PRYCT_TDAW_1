c7a17769071d4c266876fb53626ad956495f9095
Nan08 <nan.galicia03@gmail.com>
2023-05-17 08:56:19 -0600

    validación d datos inicio de sesión,falta la url

--- js/inicio_registro/validación.js
+++ js/inicio_registro/validación.js
@@ -1,11 +1,13 @@
 function validarFormulario() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    
-    if (email === "" || password.length < 6) {
-      alert("Por favor, ingrese un correo electrónico válido y una contraseña con al menos 6 caracteres.");
-      return false;
-    }
-    
-    return true;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  const vaEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const vaPassword = /^.{6,12}$/;
+
+  if (!vaEmail.test(email) || !vaPassword.test(password)) {
+    alert("Por favor, ingrese un correo electrónico válido y una contraseña con al menos 6 caracteres.");
+    return false;
   }
+
+  return true;
+}
