979df872f9005214437cceade47f1961cf734722
Daphne <daphnesofiaglez@gmail.com>
2023-05-22 09:10:34 -0600

    sin alerts

--- js/inicio_registro/registro.js
+++ js/inicio_registro/registro.js
@@ -1,24 +1,24 @@
-function validarFormulario(nombre, usuario, email, password, password2) {
-  let vaEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  let vaNombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/
-  let vaUsuario = /^[a-zA-Z0-9\_\-]{6,10}$/
-  let vaPassword = /^.{6,12}$/ 
+// function validarFormulario(nombre, usuario, email, password, password2) {
+//   let vaEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//   let vaNombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/
+//   let vaUsuario = /^[a-zA-Z0-9\_\-]{6,10}$/
+//   let vaPassword = /^.{6,12}$/ 
 
-  if(!vaNombre.test(nombre)){
-    alert("El nombre es obligatorio");
-  }
+//   if(!vaNombre.test(nombre)){
+//     alert("El nombre es obligatorio");
+//   }
 
-  if ( !vaUsuario.test(usuario) || !vaEmail.test(email) || !vaPassword.test(password2)) {
-    console.log("mensaje")
-    alert("Por favor, ingrese un usuario, correo electrónico válido y una contraseña con al menos 6 caracteres.");
-    return false;
-  }
-  if (password != password2) {
-    alert("Las contraseñas no coinciden.");
-    return false;
-    }
-  return true;
-}
+//   if ( !vaUsuario.test(usuario) || !vaEmail.test(email) || !vaPassword.test(password2)) {
+//     console.log("mensaje")
+//     alert("Por favor, ingrese un usuario, correo electrónico válido y una contraseña con al menos 6 caracteres.");
+//     return false;
+//   }
+//   if (password != password2) {
+//     alert("Las contraseñas no coinciden.");
+//     return false;
+//     }
+//   return true;
+// }
 
 //http://localhost:8081/uploads/profiles/userDefault.png
 
@@ -40,7 +40,7 @@ formulario.addEventListener('submit', async (event) => {
         password
       });
       console.log(response);
-      alert(response.data.message);
+      //alert(response.data.message);
       // Redireccionar después de mostrar el mensaje
       window.location.href = "../../inicio_sesion.html";
        // Manejar la respuesta del servidor
--- js/inicio_registro/validate.js
+++ js/inicio_registro/validate.js
@@ -1,12 +1,12 @@
-function validarFormulario(email, password) {
-  const vaEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  const vaPassword = /^.{6,12}$/;
-  if (!vaEmail.test(email) || !vaPassword.test(password)) {
-    alert("Por favor, ingrese un correo electrónico válido y una contraseña con al menos 6 caracteres.");
-    return false;
-  }
-  return true;
-}
+// function validarFormulario(email, password) {
+//   const vaEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//   const vaPassword = /^.{6,12}$/;
+//   if (!vaEmail.test(email) || !vaPassword.test(password)) {
+//     alert("Por favor, ingrese un correo electrónico válido y una contraseña con al menos 6 caracteres.");
+//     return false;
+//   }
+//   return true;
+// }
 
 // Función para iniciar sesión
 const iniciarSesion = async (correo, contrasena) => {
